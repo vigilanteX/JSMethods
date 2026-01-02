07d756c7d97a82426315a468165ea73acb829633
vigilanteX <dixitaman8@gmail.com>
2026-01-02 19:52:19 +0530

    String-utility methods

---
+++ Strings/demo5.js
@@ -0,0 +1,12 @@
+// ### 🔹 Utility
+// ```js
+// str.concat(" world")
+// str.repeat(3)
+// str.charAt(0)
+// str.charCodeAt(0)
+// ```
+let x = "amAn";
+console.log(x.concat(" dixit"));
+console.log(x.repeat(4));
+console.log(x.charAt(1));
+console.log(x.charCodeAt(2));
