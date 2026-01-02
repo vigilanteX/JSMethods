dff41a908117b9cccfa20da6444075efcf0c3f00
vigilanteX <dixitaman8@gmail.com>
2026-01-02 19:46:43 +0530

    String-replace/split

---
+++ Strings/demo4.js
@@ -0,0 +1,10 @@
+// ### 🔹 Replace / Split
+
+// ```js
+// str.replace("a", "b")
+// str.replaceAll("a", "b")
+// str.split(",")
+const x = "hello world!";
+console.log(x.replace("l", "X"));
+console.log(x.replaceAll("l", "X"));
+console.log(x.split(" "));
