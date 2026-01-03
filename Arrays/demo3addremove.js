b75e7ba5f88a7697662014532c1dadf34cf01304
vigilanteX <dixitaman8@gmail.com>
2026-01-03 12:08:02 +0530

    Array mutable methods add/remove

---
+++ Arrays/demo3addremove.js
@@ -0,0 +1,20 @@
+// ### 🔹 Add / Remove
+
+// ```js
+// arr.push()
+// arr.pop()
+// arr.shift()
+// arr.unshift()
+// arr.splice()
+let arr = [1, 2, 3, 4, 5, 6];
+// arr.push(7);
+// console.log(arr);
+
+// arr.pop();
+// console.log(arr);
+
+//arr.shift():removes first element
+arr.shift();
+console.log(arr);
+arr.unshift(100);
+console.log(arr);
