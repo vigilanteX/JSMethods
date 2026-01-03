7b0acdbc1832cdae488a923895c4b8b18b9bb276
vigilanteX <dixitaman8@gmail.com>
2026-01-03 12:23:07 +0530

    Array mutable methods -splice()

--- Arrays/demo3addremove.js
+++ Arrays/demo3addremove.js
@@ -15,6 +15,23 @@ let arr = [1, 2, 3, 4, 5, 6];
 
 //arr.shift():removes first element
 arr.shift();
-console.log(arr);
+//console.log(arr);
 arr.unshift(100);
-console.log(arr);
+//console.log(arr);
+
+let arr2 = [1, 2, 3, 4, 5, 6, 7, 8];
+/*
+Parameters
+
+startIndex
+Index at which to start changing the array
+(negative values count from the end)
+deleteCount
+Number of elements to remove
+0 → remove nothing
+omitted → remove all elements from startIndex onward
+itemsToAdd (optional)
+Elements to insert at startIndex
+*/
+//console.log(arr2.splice(3));-delete all elements 3rd index onwards
+//console.log(arr2.splice(0, 0, 9, 9, 9));-at zeroth index, delete nothing, start adding 9,9,9 starting zeroth index
